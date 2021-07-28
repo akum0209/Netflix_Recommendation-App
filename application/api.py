e3a4404bd6ce8f275b795354270985072b3b3ce7
abhinav <abhi.abhinav0209@gmail.com>
2021-07-28 23:04:22 +0100

    Creating  GET APIs

--- application/api.py
+++ application/api.py
@@ -1,10 +1,67 @@
 from application.app import app
+from flask import request
 
 # Import your models here
-from application.models import User
+#from application.models import User
+from application.models import movie_list
+from application.models import series_list
+
 
 @app.route("/")
 def home():
-    return {"Status": "Success"}, 200 
+    return {"Status": "Success"}, 200
+
 
 # Write your API endpoints here
+@app.route("/movie", methods=["Get"])
+def get_movie_list():
+    print(request.args)
+    country = request.args["countrylist"]
+    # method 1
+    try:
+        genre = request.args["genreid"]
+    except KeyError:
+        genre  =None
+
+
+    # Method 2
+    #genre = request.args.get("genreid", None)
+
+
+    #genre = request.args["genreid"]
+    print(type(country))
+    m_list = movie_list(country, genre)
+    print(type(m_list["results"]))
+    l = []
+    for i in m_list["results"]:
+        print(i["title"], ',', i["imdbrating"],',',  i["year"])
+        new_dict = {'title': i["title"], 'imdbrating': i["imdbrating"], 'year': i["year"]}
+        l.append(new_dict)
+    print(type(l))
+    print(l)
+    movies = {'results':l}
+    return movies
+# l
+
+@app.route("/series", methods=["Get"])
+def get_series_list():
+    print(request.args)
+    country = request.args["countrylist"]
+    try:
+        genre = request.args["genreid"]
+    except KeyError:
+        genre  =None
+
+#    genre = request.args["genreid"]
+    print(type(country))
+    s_list = series_list(country, genre)
+    print(type(s_list["results"]))
+    l = []
+    for i in s_list["results"]:
+        print(i["title"], ',', i["imdbrating"],',',  i["year"])
+        new_dict = {'title': i["title"], 'imdbrating': i["imdbrating"], 'year': i["year"]}
+        l.append(new_dict)
+    print(type(l))
+    print(l)
+    series = {'results': l}
+    return series
--- application/models.py
+++ application/models.py
@@ -1,25 +1,26 @@
-from application.app import app, db
+def movie_list(countrylist,genreid):
+    import requests
+    url = "https://unogsng.p.rapidapi.com/search"
+    headers = {
+        'x-rapidapi-key': "a967acffecmsh5456f5c23c3b388p16cdb5jsnd021e4b13a88",
+        'x-rapidapi-host': "unogsng.p.rapidapi.com"
+    }
+
+    querystring = {"countrylist": countrylist, "genrelist": genreid, "orderby":"rating", "type":"movie"}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    return response.json()
+
+def series_list(countrylist,genreid):
+    import requests
+    url = "https://unogsng.p.rapidapi.com/search"
+    headers = {
+        'x-rapidapi-key': "a967acffecmsh5456f5c23c3b388p16cdb5jsnd021e4b13a88",
+        'x-rapidapi-host': "unogsng.p.rapidapi.com"
+    }
+
+    querystring = {"countrylist": countrylist, "genrelist": genreid, "orderby":"rating", "type":"series"}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    return response.json()
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    # email = db.Column(db.String(80), unique=True, nullable=False)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120))
 
-
-# Sample for One-many and Many-Many relationship
-# class WorkItem(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(80), nullable=False)
-#     description = db.Column(db.String(120))
-
-#     created_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-#     shared_with = db.relationship('User', secondary=shared_with, lazy='subquery', backref=db.backref('user', lazy=True))
-
-# shared_with = db.Table('shared_with',
-#     db.Column('work_item_id', db.Integer, db.ForeignKey('work_item.id'), primary_key=True),
-#     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
-# )
-
-# Using the above sample write the DB models here
