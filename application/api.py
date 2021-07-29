43d9726437d335efd570bcf58b2176a3615996ed
akum0209 <abhi.abhinav0209@gmail.com>
2021-07-29 12:02:01 +0100

    Creating  GET APIs , working on authentication , parameterising the keys

--- application/api.py
+++ application/api.py
@@ -1,5 +1,8 @@
 from application.app import app
 from flask import request
+from application.models import WorkItem, db, SubTask, User
+from sqlalchemy.exc import IntegrityError
+from flask_jwt import jwt_required, JWT, current_identity
 
 # Import your models here
 #from application.models import User
@@ -7,10 +10,35 @@ from application.models import movie_list
 from application.models import series_list
 
 
+def identity(payload):
+    user_id = payload['identity']
+    print(user_id)
+    return User.query.filter_by(id=user_id).first()
+
+
+def authenticate(username, password):
+    user = User.query.filter_by(username=username).first()
+    if user and user.password == password:
+        return user
+
+
+jwt = JWT(app, authenticate, identity)
+
+
 @app.route("/")
 def home():
     return {"Status": "Success"}, 200
 
+@app.route("/signup", methods=["POST"])
+def signup():
+    params = request.authorization
+    try:
+        user = User(**params)
+        db.session.add(user)
+        db.session.commit()
+    except IntegrityError:
+        return {"Status": "Error", "result": "User already exists"}, 400
+    return {"Status": "Success", "result": "User created"}
 
 # Write your API endpoints here
 @app.route("/movie", methods=["Get"])
--- application/models.py
+++ application/models.py
@@ -1,9 +1,11 @@
+from application.app import app, db
+import os
 def movie_list(countrylist,genreid):
     import requests
     url = "https://unogsng.p.rapidapi.com/search"
     headers = {
-        'x-rapidapi-key': "a967acffecmsh5456f5c23c3b388p16cdb5jsnd021e4b13a88",
-        'x-rapidapi-host': "unogsng.p.rapidapi.com"
+        'x-rapidapi-key': os.environ["Rapid_key"],
+        'x-rapidapi-host': os.environ["Rapid_host"]
     }
 
     querystring = {"countrylist": countrylist, "genrelist": genreid, "orderby":"rating", "type":"movie"}
@@ -14,13 +16,51 @@ def series_list(countrylist,genreid):
     import requests
     url = "https://unogsng.p.rapidapi.com/search"
     headers = {
-        'x-rapidapi-key': "a967acffecmsh5456f5c23c3b388p16cdb5jsnd021e4b13a88",
-        'x-rapidapi-host': "unogsng.p.rapidapi.com"
+        'x-rapidapi-key': os.environ["Rapid_key"],
+        'x-rapidapi-host': os.environ["Rapid_host"]
     }
 
     querystring = {"countrylist": countrylist, "genrelist": genreid, "orderby":"rating", "type":"series"}
     response = requests.request("GET", url, headers=headers, params=querystring)
     return response.json()
 
+def country():
+    import requests
+    url = "https://unogsng.p.rapidapi.com/countries"
 
+    headers = {
+        'x-rapidapi-key': os.environ["Rapid_key"],
+        'x-rapidapi-host': os.environ["Rapid_host"]
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    with open('countries.csv', 'w') as fd:
+        #for chunk in response.iter_content(chunk_size):
+        fd.write(response.text)
+
+def genre():
+    import requests
+    url = "https://unogsng.p.rapidapi.com/genres"
+
+    headers = {
+        'x-rapidapi-key': os.environ["Rapid_key"],
+        'x-rapidapi-host': os.environ["Rapid_host"]
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    with open('genre.csv', 'w') as fd:
+        #for chunk in response.iter_content(chunk_size):
+        fd.write(response.text)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # email = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120))
+
+
+country()
+genre()
 
