327e10aa0c4144f891a2c3a8a4d2fd616635f3cb
akum0209 <abhi.abhinav0209@gmail.com>
2021-07-30 13:49:02 +0100

    Creating  GET APIs , working on authentication , parameterising the keys

--- application/models.py
+++ application/models.py
@@ -1,5 +1,22 @@
 from application.app import app, db
 import os
+
+country_dict = {'Australia': 89, 'Canada': 33, 'United Kingdom': 46, 'United States': 78, 'India': 337}
+#print(country_dict)
+
+genre_dict = {'Sports Documentaries': 180, 'Independent Dramas': 384, 'Anime Dramas':452,
+              'Children & Family Films': 783, 'Critically-acclaimed Independent Films': '875',
+              'Critically-acclaimed Action & Adventure': 899, 'Steamy Thrillers': 972,
+              'Sci-Fi & Fantasy': 1492, 'Korean Drama Movies': 1989,
+              'Courtroom Dramas': 2748, 'Independent Thrillers': 3269, 'Romantic Films based on a book': 3830,
+              'Sitcoms': 3903, 'Sci-Fi Dramas': 3916, 'Critically Acclaimed Films': 3979,
+              'Animation': 4698, 'Indian Dramas': 5051, 'Bollywood Films': 5480, 'Documentaries': 6839,
+              'Indian Comedies': 9942, 'Political Thrillers': 10504, 'Sci-Fi Thrillers': 11014,
+              'Stand-up Comedy': 11559, 'Action Thrillers': 43048
+              }
+
+#print(genre_dict)
+
 def movie_list(countrylist,genreid):
     import requests
     url = "https://unogsng.p.rapidapi.com/search"
@@ -63,3 +80,5 @@ class User(db.Model):
 
 db.create_all()
 db.init_app(app)
+
+
